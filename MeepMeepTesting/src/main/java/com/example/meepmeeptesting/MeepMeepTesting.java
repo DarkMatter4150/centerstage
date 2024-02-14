860d55aa15d28e8af5fd95f2e69a55945ae60743
Alex Jannusch <akjannusc@gmail.com>
2024-02-13 20:31:43 -0600

    Updated Support Traj for Delta Scrimmage

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -15,12 +15,12 @@ import static java.lang.Thread.sleep;
 public class MeepMeepTesting {
     static Pose2d startPoseMC = new Pose2d(12, 60, Math.toRadians(180));
     //Start Position for Main Character Robot
-    static Pose2d startPoseSupport = new Pose2d(-36, 60, Math.toRadians(180));
+    static Pose2d startPoseSupport = new Pose2d(-41, 62, Math.toRadians(270));
     //Start Position for Support Robot
 
     static Vector2d[] tapesBlueLeft = { new Vector2d(9, 30), new Vector2d(15, 24), new Vector2d(31, 30)};
     //Location of tape spikes by stacks (Blue)
-    static Vector2d[] tapesBlueRight = { new Vector2d(-24, 33), new Vector2d(-36, 24), new Vector2d(-48, 30)};
+    static Vector2d[] tapesBlueRight = { new Vector2d(-33, 33), new Vector2d(-40, 24), new Vector2d(-55, 30)};
     //Location of tape spikes closest to backboard (Blue)
     static Vector2d[] boardsBlue = { new Vector2d(48, 30), new Vector2d(48, 36), new Vector2d(48, 42)};
     //Location of Backboard for Blue Alliance
@@ -39,7 +39,7 @@ public class MeepMeepTesting {
 
 
     public static void main(String[] args) throws InterruptedException {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(500);
         //New MeepMeep instance
         //Dark Matter Robot
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
@@ -56,18 +56,40 @@ public class MeepMeepTesting {
                 .build();
 //Support Role (US)
         myBot.runAction(myBot.getDrive().actionBuilder(startPoseSupport)
-                .strafeToConstantHeading(stacksBlue[1])
-                //Intake code goes here
-                .strafeToConstantHeading(new Vector2d(-18, 12))
-                .splineToConstantHeading(new Vector2d(48, 18),0)
-                .strafeTo(boardsBlue[loc])
-                //Outtake code goes here
+                                .strafeToConstantHeading(new Vector2d(-36, 36))
+                                .strafeToConstantHeading(tapesBlueRight[0])
+                                .strafeToConstantHeading(new Vector2d(-36, 36))
+
+
+
+//                    .strafeToLinearHeading(tapesBlueRight[0], 220)
+//                    .strafeToLinearHeading(new Vector2d(-36, 10), 0)
+//                    .strafeToConstantHeading(new Vector2d(48, 18))
+//                    .strafeTo(boardsBlue[loc])
+          //Use for state
+                //Coords for state
+                // tapes
+                //0 -33, 33
+                //1 -40, 24
+                //2 -60, 12
+
+
+
+//                .strafeToConstantHeading(stacksBlue[1])
+//                //Intake code goes here
+////                .strafeToConstantHeading(new Vector2d(-18, 12))
+//////                .splineToConstantHeading(new Vector2d(48, 18),0)
+//////                .strafeTo(boardsBlue[loc])
+
+//////                //Outtake code goes here
                 .build());
 //Main Character Role (Ally)
         alliedBot.runAction(alliedBot.getDrive().actionBuilder(startPoseMC)
                         .strafeTo(tapesBlueLeft[loc])
-                        .strafeToConstantHeading(boardsBlue[loc])
+                        .strafeToConstantHeading(boardsBlue[2])
                         .strafeTo(new Vector2d(48, 36))
+                        .strafeTo(stacksBlue[0])
+
 
                         .build());
 
@@ -84,7 +106,7 @@ public class MeepMeepTesting {
 
 
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
